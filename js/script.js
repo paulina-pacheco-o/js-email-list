0bda938ab876e48a33fb38827d153d2265de86b9
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-18 15:14:52 +0200

    email console

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//const list = document.getElementById(`lista`);
+
+for (let i = 0; i < 10; i++) {
+  axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
+    console.log(resp.data.response)
+  })
+}
